6385e344d962453b4fbe19163d7a12bc1e4fbf16
Paul Pereira <paul.pereira@hotmail.com>
2016-03-13 23:23:25 -0400

    student3

--- src/student_player/StudentPlayer3.java
+++ src/student_player/StudentPlayer3.java
@@ -6,7 +6,7 @@ import hus.HusMove;
 
 import java.util.ArrayList;
 
-import student_player.mytools.MyTools;
+import student_player.mytools.TreeNode;
 
 /** A Hus player submitted by a student. */
 public class StudentPlayer3 extends HusPlayer {
@@ -23,73 +23,30 @@ public class StudentPlayer3 extends HusPlayer {
      * for another example agent. */
     public HusMove chooseMove(HusBoardState board_state)
     {
-        int bestmove = minimax((HusBoardState)board_state.clone(),true,0,4,0,73);
+        int bestmove = mcsTree((HusBoardState)board_state.clone());
         return board_state.getLegalMoves().get(bestmove);
 
         // But since this is a placeholder algorithm, we won't act on that information.
     }
 
-    public int minimax(HusBoardState board,boolean max,int depth,int maxdepth,int alpha,int beta){
-        if(depth < maxdepth){
-            ArrayList<HusMove> moves = board.getLegalMoves();
-            if(moves.size()==0 && max){
-                return 0;
-            }
-            else if(moves.size()==0 && !max){
-                return 73;
-            }
-            HusBoardState current;
-            int bestmove = 0;
-            int currentvalue;
-            for(int i=0;i<moves.size();i++){
-                current = (HusBoardState)board.clone();
-                current.move(moves.get(i));
-                currentvalue = minimax(current,!max,depth+1,maxdepth,alpha,beta);
-                if(max){
-                    if(alpha<currentvalue){
-                        alpha = currentvalue;
-                        bestmove = i;
-                    }
-                    if(alpha>=beta){
-                        if(depth == 0){
-                            return i;
-                        }
-                        else{
-                            return beta;
-                        }
-                    }
-                }
-                else{ 
-                    if(beta>currentvalue){
-                        beta = currentvalue;
-                    }
-                    if(alpha>=beta){
-                        return alpha;
-                    }
-                }
-            }
-            if(depth == 0){
-                return bestmove;
-            }
-            else{
-                if(max){
-                    return alpha;
-                }
-                else{
-                    return beta;
-                }
-            }
-        }
-        else{
-            return score(board.getPits()[player_id]);
+    public int mcsTree(HusBoardState board,int iteration){
+        double C = 1;
+        TreeNode head = new TreeNode(null);
+        TreeNode leaf;
+        HusBoardState current;
+        for(int i=0;i<iteration;i++){
+            current = (HusBoardState)board.clone();
+            leaf = selection(current,head,C);
         }
     }
 
-    public int score(int[] pits){
-        int score = 0;
-        for(int i=0;i<pits.length;i++){
-            score += pits[i];
+    public TreeNode selection(HusBoardState board,TreeNode node,double C){
+        if(node.getNumberChild()==0){
+            return node;
         }
-        return score;
+        else{
+            board.move(board.getLegalMove().get(node.getMove()));
+            return selection(node.getBestChild(C));
+        }
     }
 }
